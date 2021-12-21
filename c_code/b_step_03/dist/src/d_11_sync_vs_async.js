59bc036761337a9b0416c038c9919d85e6b3ab23
SA K <sakang07@gmail.com>
2021-12-21 12:55:16 +0900

    [feat] slide 예제 3

--- c_code/b_step_03/dist/src/d_11_sync_vs_async.js
+++ c_code/b_step_03/dist/src/d_11_sync_vs_async.js
@@ -1,25 +1,44 @@
-// d_11_sync_vs_async.js
+// d_11_sync_vs_Async.js
 
 let num = 0;
 
 /*
 // 순서의 흐름대로 처리
-console.log(num);
-console.log(num += 1);
-console.log(num += 3);
-console.log(num += 5);
-console.log(num);
+console.log('1:', num );
+console.log('2:', num += 1 );
+console.log('3:', num += 3 );
+console.log('4:', num += 5 );
+console.log('5:', num );
 */
 
-console.log('1: ', num);
-console.log('2: ', num += 1);
 
-setTimeout(() => {
-  console.log('3: ', num += 3);
-}, 100);
+/* 
+// 만들어진 코드의 순서대로 진행? , 적용가능한 코드먼저 처리 후 추가 작업
+console.log('1:', num );
+console.log('2:', num += 1 );
+setTimeout( () => { console.log('3:', num += 3 );} , 100);
+setTimeout( ()=> {  console.log('4:', num += 5 );}, 300);
+console.log('5:', num );
+*/
 
-setTimeout(() => {
-  console.log('4: ', num += 3);
-}, 300);
+const fnAge = age => {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 20) {
+        resolve('성인입니다')
+      } else {
+        reject('미성년자입니다');
+      }
+    }, 1000)
+  });
+}
 
-console.log('5: ', num);
+fnAge(22)
+  .then(console.log)
+  .catch(console.log)
+  .finally(() => {
+    console.log('promise의 개념 이해')
+  });
+
+
+  
