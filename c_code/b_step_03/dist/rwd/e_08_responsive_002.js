eb9965c880c45b58bd45df5f7837b6b72b747daa
SA K <sakang07@gmail.com>
2021-12-28 15:28:37 +0900

    [feat] responsive web, hover, focus

---
+++ c_code/b_step_03/dist/rwd/e_08_responsive_002.js
@@ -0,0 +1,66 @@
+// e_08_responsive_002.js
+
+
+// 1. headBox내부에 있는 gnb영역과, nav_btn영역 선택
+// 2. mobile버전에서는  nav_btn영역이 존재, pc버전에서는 존재가 필요없으므로 삭제
+{
+  
+  // 선택자 변수
+  const elBody     = document.querySelector('body');
+  const elHeadBox  = document.querySelector('#headBox');
+  const elNavBox   = document.querySelector('#navBox');
+  const elNavTitle = elNavBox.querySelector('h2');
+  const elNavBtn   = elNavBox.querySelector('.nav_btn');
+  const elGnb      = elNavBox.querySelector('.gnb');
+
+  // 각 디바이스 세팅
+  // const setDevice = [
+  //   {type:'mobile', size:600},
+  //   {type:'tablet', size:1024},
+  //   {type:'laptop', size:1440},
+  //   {type:'pc'}
+  // ];
+
+  const setDevice = [ 
+    {size:768, type:'mobile', nav: '../temp/e_08_rwd_mob.html', script:'../dist/rwd/e_08_rwd_nav_mobile.js'},
+    {          type:'pc',     nav: '../temp/e_08_rwd_pc.html', script:'../dist/rwd/e_08_rwd_nav_pc.js'}
+  ];
+
+  const mediaSize = `screen and (max-width:${setDevice[0].size}px)`;
+  const mediaMatches = window.matchMedia(mediaSize);
+  // ---------------------------------------------------------------
+  // 기능 함수
+
+  const fnMkScript = (data)=>{
+    const ckScript = document.querySelector('.navScript');
+    if(ckScript !== null){
+      ckScript.remove();
+    }
+
+    const mkScript = document.createElement('script');
+    mkScript.setAttribute('src', data);
+    mkScript.setAttribute('class', 'navScript');
+    elBody.append(mkScript);
+  };
+
+  const fnCheckHeader = (type = mediaMatches.matches)=>{
+    let num = type ? 0 : 1; // 변수 = 삼항연산자로 넣으면 true인 항을 변수에 할당하는 것
+
+    fetch(setDevice[num].nav)
+      .then(response => response.text())
+      .then(data => {elNavBox.innerHTML = data})
+      .then(() => { fnMkScript(setDevice[num].script)});
+  };
+  
+
+
+  fnCheckHeader(mediaMatches.matches);
+  // -----------------------------------------------------------
+  // 이벤트 체크
+  mediaMatches.addEventListener('change', (e)=>{
+    // location.reload();
+    fnCheckHeader(e.matches);
+  });
+
+}
+
